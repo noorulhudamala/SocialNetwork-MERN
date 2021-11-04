0dd28527dc9fc91e278879d6c923a9d901426c23
Noor Ul Huda <noorulhuda.mala@gmail.com>
2021-11-04 11:00:30 +0500

    Moogose Connection

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 
-const gql = require("graphql-tag")
+const Post = require("./models/Post");
+const { MONGODB } = require("./config.js");
+
 const typeDefs = gql`
-    type Query{
-        sayHi: String!
-    }
-`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    userName: String!
+  }
+  type Query {
+    getPosts: [Post]
+  }
+`;
 
 const resolvers = {
-    Query: {
-        sayHi: () => "Hello World" 
-    }
-    
-}
+  Query: {
+    getPosts: async () => {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 });
 
-server.listen({ port: 5000 })
-    .then(res => {
-    console.log(`server running at ${res?.url}`)
-})
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`server running at ${res.url}`);
+  });
---
+++ models/Post.js
@@ -0,0 +1,25 @@
+const { model, Schema } = require("mongoose");
+const postSchema = new Schema({
+    body: String,
+    userName: String,
+    createdAt: String,
+    comments: [
+        {
+            body: String,
+            userName: String,
+            CreatedAt: String
+        }
+    ],
+    likes: [
+        {
+            userName: String,
+            createdAt: String
+        }
+    ],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    }
+})
+
+module.exports = model("Post", postSchema);
