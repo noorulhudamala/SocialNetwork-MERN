8360b4532a1422cbbd30d32be6f8067002d993e4
Noor ul Huda <noorulhuda.mala@gmail.com>
2021-11-08 21:52:52 +0500

    basic menu and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import React from 'react';
+
+
+import MenuBar from './components/MenuBar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
@@ -9,12 +13,17 @@ import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/register" component={Register}/>
-      <Route exact path="/login" component={Login}/>
+      <div className="ui container">
+        <MenuBar />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,36 +1,44 @@
 
 import React, { useState } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
+import { Menu } from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
+
 
 const MenuBar = () => {
 
-    const [activeItem, setActiveItem] = useState()
+    const pathName = window.location.pathname;
+    const path = pathName === "/" ? "home" : pathName.substr(1)
+
+    const [activeItem, setActiveItem] = useState(path)
     const handleItemClick = (e, { name }) => setActiveItem(name)
     return (
-            <Menu pointing secondary>
+        <Menu pointing secondary size="massive" color="teal">
+            <Menu.Item
+                name='Home'
+                active={activeItem === 'home'}
+                onClick={handleItemClick}
+                as={Link}
+                to= "/"
+            />
+
+
+            <Menu.Menu position='right'>
                 <Menu.Item
-                    name='Home'
-                    active={activeItem === 'home'}
+                    name='login'
+                    active={activeItem === 'login'}
                     onClick={handleItemClick}
+                    as={Link}
+                    to="/login"
                 />
                 <Menu.Item
-                    name='messages'
-                    active={activeItem === 'messages'}
+                    name='register'
+                    active={activeItem === 'register'}
                     onClick={handleItemClick}
+                    as={Link}
+                    to="/register"
                 />
-                <Menu.Item
-                    name='friends'
-                    active={activeItem === 'friends'}
-                    onClick={handleItemClick}
-                />
-                <Menu.Menu position='right'>
-                    <Menu.Item
-                        name='logout'
-                        active={activeItem === 'logout'}
-                        onClick={handleItemClick}
-                    />
-                </Menu.Menu>
-            </Menu>
+            </Menu.Menu>
+        </Menu>
     )
 }
 export default MenuBar;
